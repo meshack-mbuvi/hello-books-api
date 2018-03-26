f5112299e8c73ea95e8c0cc3c0e9136483120104
Prince <=>
2018-03-26 17:06:04 +0300

    [Feature #156246075] Add trailing slash to the url in test_edit_item function in tests.py file.

--- application/books/views.py
+++ application/books/views.py
@@ -32,6 +32,8 @@ class books(Resource):
     def generateID(self):
         # Get the ids already assigned and sort them in ascending order
         items = [book.id for book in books_in_api]
+        if len(items) == 0:
+            return 1
         items.sort()
         newID = items[-1] + 1
         return newID
@@ -57,6 +59,7 @@ class books(Resource):
         # add new book object now
         books_in_api.append(book)
 
+
         # format data to be returned to the calling client
         data = self.make_response(book)
 
@@ -73,4 +76,13 @@ class books(Resource):
         # delete the item from the list
         books_in_api.remove(books[0])
 
+        return self.get()
+
+    def put(self):
+        # confirm we have the right format and required fields
+        # if not request.json or 'author' not in request.json or 'title' not in request.json:
+        #     abort(400)
+
+        
+        
         return 200
