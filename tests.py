17576c58c130c96ae792dbcae1e356626b30b0fa
Prince <=>
2018-03-26 16:11:51 +0300

    [Feature #156246075] Add testcases for edit endpoint.

--- tests.py
+++ tests.py
@@ -106,6 +106,7 @@ class BookAPITests(unittest.TestCase):
 
     def test_edit_item(self):
         '''This endpoint updates information for a given book'''
+
         new_info = {'id': 1, 'title': 'Learn Java the Hard way',
                     'author': 'Meshack'}
         resp = self.app.put(self.BASE_URL, data=json.dumps(
@@ -172,6 +173,20 @@ class UserTests(unittest.TestCase):
 
         recv_data = json.loads(resp.get_data().decode('utf-8'))
         password = recv_data['password']
+
+        new_info = {'title': 'Learn Java the Hard way',
+                'author': 'Meshack'}
+        item_id = 1
+        resp = self.app.put(self.BASE_URL + '%d/' % item_id, data=json.dumps(
+            new_info), content_type='application/json')
+
+        self.assertEqual(resp.status_code, 200, msg = "Endpoint should be reachable")
+
+        data = json.loads(resp.get_data().decode('utf-8'))
+        result = [{'id': data.id, 'title': data.title, 'author':data.author}]
+
+        self.assertTrue({'id':1,'title': 'Learn Java the Hard way',
+                'author': 'Meshack'} in result, msg = 'Should update the information for specified book')
 
         self.assertTrue(password == 'meshack',msg = "Should change users password")
 
