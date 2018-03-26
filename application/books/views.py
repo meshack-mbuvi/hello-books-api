4c677b48ae70735425a24be6619a697ddfff8fdb
Prince <=>
2018-03-26 12:52:54 +0300

    [Feature #156223869] Add code for the functionality of the endpoint POST /api/books

--- application/books/views.py
+++ application/books/views.py
@@ -1,5 +1,6 @@
 from application.books.models import Book
 from flask_restful import Resource
+from flask import request
 
 
 books_in_api = []
@@ -31,7 +32,39 @@ class books(Resource):
                 # book not found
                 return 'Books not found', 404
             for book in books_in_api:
-                items.append({'id': book.id, 'title': book.title, 'author': book.author})
+                items.append(
+                    {'id': book.id, 'title': book.title, 'author': book.author})
             return (items), 200
 
+    def generateID(self):
+        # Get the ids already assigned and sort them in ascending order
+        items = [book.id for book in books_in_api]
+        items.sort()
+        newID = items[-1] + 1
+        return newID
 
+    def make_response(self, Book):
+        data = {'id': Book.id, 'title': Book.title, 'author': Book.author}
+
+        return data
+
+    def post(self):
+        # confirm we have the right format and required fields
+        if not request.json or 'author' not in request.json or 'title' not in request.json:
+            abort(400)
+
+        # Get next id for the book
+        book_id = self.generateID()
+        title = request.json['title']
+        author = request.json['author']
+
+        # create new book object
+        book = Book(book_id, title, author)
+
+        # add new book object now
+        books_in_api.append(book)
+
+        # format data to be returned to the calling client
+        data = self.make_response(book)
+
+        return (data), 200
