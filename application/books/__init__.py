1e8c95cd3ffe6e5819bf2c9851a9a43c070660dc
Prince <=>
2018-03-26 11:01:52 +0300

    Restructure project to use Blueprints. Add imports for specific modules and initialize the app for basic functionalities.

--- application/books/__init__.py
+++ application/books/__init__.py
@@ -1,4 +1,12 @@
 from flask import Blueprint
-books = Blueprint('books', __name__, url_prefix='/books')
+book = Blueprint('books', __name__, url_prefix='/api/v1/books/')
 
-from application.books.views import *
+from application.books.views import books
+from application.books import models
+
+from application import app
+from flask_restful import Api
+
+api = Api(app)
+
+api.add_resource(books,'/api/v1/books/')
