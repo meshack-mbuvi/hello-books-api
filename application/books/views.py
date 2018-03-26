a594b0d5542776f6675d07346a2854b1dbe1fe15
Prince <=>
2018-03-26 12:04:13 +0300

    [Feature #156252469] Implement the functionality for GET /api/books/<bookId> endpoint.

--- application/books/views.py
+++ application/books/views.py
@@ -13,18 +13,25 @@ books_in_api.append(bk1)
 
 class books(Resource):
 
-    def get(self):
-        # items to return
-        items = []
+    def get(self, id=''):
+        if id != '':
+            # items to return
+            items = []
+            # find the specific item
+            items = [book for book in books_in_api if book.id == id]
+            if len(items) < 1:
+                # book not found
+                return 'Item not found', 404
+            return ({'Book': {'id': items[0].id, 'title': items[0].title, 'author': items[0].author}}), 200
 
-        if len(books_in_api) < 1:
-            # book not found
-            return 'Books not found', 404
-
-        for book in books_in_api:
-            items.append(
-                {'id': book.id, 'title': book.title, 'author': book.author})
-
-        return (items), 200
+        else:
+            # items to return
+            items = []
+            if len(books_in_api) < 1:
+                # book not found
+                return 'Books not found', 404
+            for book in books_in_api:
+                items.append({'id': book.id, 'title': book.title, 'author': book.author})
+            return (items), 200
 
 
