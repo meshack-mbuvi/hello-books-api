509f3a36dfce3b13382a2e9859523c9bac86093d
mbuvi <meshmbuvi@gmail.com>
2018-03-27 16:02:26 +0300

    Update views.py
    Correct code for this branch; it was for a different branch.

--- application/auth/views.py
+++ application/auth/views.py
@@ -7,14 +7,21 @@ from application.users.models import User
 # A list for holding users
 users_table = []
 
-class Register(Resource):
-	# This resource creates a new user account
+class Reset(Resource):
 	def post(self):
-		# create new user here
+		# Confirm the right fields are filled 
+		if 'username' not in request.json or 'new_password' not in request.json:
+			return {"Message":"Make sure to fill all required fields"}
 
-		user = User(username = 'meshack',password = 'password')
-		users_table.append(user)
+		username = request.json['username']
+		password = request.json['new_password']
+		# Get the user with given username
+		user = [user for user in users_table if user.username == username]
+		if not user:
+			return {"Message":"No user found with that username"}
 
-		return {'user details':{'username':user.username,'borrowings':user.borrowed_books}}
+		users_table.remove(user[0])
+		user[0].password = password
+		users_table.append(user[0])
 
-
+return {"username":user[0].username,"password":user[0].password
