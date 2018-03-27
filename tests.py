39977024bb38f8c24f667fc03953e448304ad5b1
Prince <=>
2018-03-27 14:01:05 +0300

    [Feature #156303705] Add class Reset to auth/views.py with a post method that resets user's password.

--- tests.py
+++ tests.py
@@ -129,8 +129,10 @@ class UserTests(unittest.TestCase):
         # Prepare for testing;set up variables
         from application.auth.views import users_table
         from application.users.models import User
+        self.user = User(username="mbuvi", password="mesh")
 
-        users_table.append(User(username="mbuvi",password="mesh"))
+        users_table.append(self.user)
+
 
         self.users_table = users_table
 
@@ -145,6 +147,8 @@ class UserTests(unittest.TestCase):
         self.app.testing = False
         self.app = None
         self.BASE_URL = None
+        self.users_table = None
+
 
     def test_can_create_user(self):
         initial_number = len(self.users_table)
@@ -158,7 +162,18 @@ class UserTests(unittest.TestCase):
         self.assertTrue(number_after_user_created > initial_number,
                         msg="user should be created and added to system")
 
-    
+    def test_user_can_change_password(self):
+        data = {"username": "mbuvi", "new_password": "meshack"}
+
+        resp = self.app.post(self.BASE_URL + 'reset',
+                             data=json.dumps(data), content_type='application/json')
+        if resp.status_code != 200:
+            return 1
+
+        recv_data = json.loads(resp.get_data().decode('utf-8'))
+        password = recv_data['password']
+
+        self.assertTrue(password == 'meshack',msg = "Should change users password")
 
 if __name__ == '__main__':
     unittest.main()
