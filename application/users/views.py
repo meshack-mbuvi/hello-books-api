ce4ec070eeae962e2cb09cbf2cb9aadf75e8d07f
mbuvi <meshmbuvi@gmail.com>
2018-03-30 09:09:04 +0300

    Update views.py
    Remove duplication of classes resulting from merged branches

--- application/users/views.py
+++ application/users/views.py
@@ -10,32 +10,6 @@ from application.auth.views import users_table
 
 class Users(Resource):
 
-	def get(self, username=None):
-		# TODO: Return all the books the user has borrowed
-
-
-
-		return {'message':'Welcome here. You can borrow books now'},200
-
-class Borrow(Resource):
-	def post(self):
-		# This method is used for borrowing a new book
-		# Let us load the book with the id given
-		item_id = request.json['id']
-		book = [item for item in books_in_api if item.id == item_id]
-		book[0].available = False
-
-		username = request.json['username']
-
-		user = [user for user in users_table if user.username == username]
-
-		user[0].borrowed_books.append(book)
-
-		return ({'User':user[0].username,"borrowings":user[0].borrowed_books}),201
-
-
-class Users(Resource):
-
     def get(self, username=None):
         # TODO: Return all the books the user has borrowed
 
