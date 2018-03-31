67d7ef0a4d3574a8de645488aa3902b9af96c931
Prince <=>
2018-03-31 10:31:09 +0300

    Move all initialization code to __init__.py file of the application.

--- run.py
+++ run.py
@@ -1,16 +1,4 @@
-
-# Blueprint names
-from application.books import book
-from application.users import user
-from application.auth import auth
-
 from application import app
-
-# Register the blueprints
-app.register_blueprint(book)
-app.register_blueprint(user)
-app.register_blueprint(auth)
-
 
 if __name__ == '__main__':
 	app.run(debug = True)
