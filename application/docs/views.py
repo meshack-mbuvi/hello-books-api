674237631f66b7c09a4abbf0a495df9d924c8aca
Prince <=>
2018-03-31 12:48:24 +0300

    Update docs configuration

--- application/docs/views.py
+++ application/docs/views.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
 
-docs = Blueprint('docs', __name__, template_folder = 'templates')
+docs = Blueprint('docs', __name__, static_folder='static', template_folder = 'templates')
 
 @docs.route('/')
 def index():
 	"Show an index template"
 
-	return render_template('docs/index.html')
+	return render_template('index.html')
