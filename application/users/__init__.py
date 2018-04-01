6f34d4021e7fb450b9f6ba6dbbd8febc288e342d
Prince <=>
2018-04-01 19:02:52 +0300

    [Feature #156306687] Delete imports not needed in the app

--- application/users/__init__.py
+++ application/users/__init__.py
@@ -3,11 +3,9 @@ from flask import Blueprint
 user = Blueprint('users', __name__, url_prefix='/api/v1/users/')
 
 from application.users.borrowbook import  Borrow
-from application.users.views import Users
 
 from application import app
 from flask_restful import Api
 
 api = Api(app)
-api.add_resource(Users, '/api/v1/users/')
 api.add_resource(Borrow, '/api/v1/users/books/')
