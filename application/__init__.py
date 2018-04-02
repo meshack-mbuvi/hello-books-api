6672c6b9f5fb7974e7bd58423603ee594260e360
Prince <=>
2018-04-02 17:32:54 +0300

    Import modules from flask-jwt-extended for use in user authentication

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,10 @@
 
 from flask import Flask
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 app = Flask(__name__)
 
 from instance.config import configuration
+jwt = JWTManager(app)
 
 users_table = {}
 books_in_api = {}
