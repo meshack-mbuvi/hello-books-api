c2e89078015138da5619db56f8d285e2994c363e
Prince <=>
2018-04-03 22:22:27 +0300

    [Feature #156479106] Modify method for borrowing a book so as it modifies the field on the book.

--- application/users/borrowbook.py
+++ application/users/borrowbook.py
@@ -8,7 +8,7 @@ from application import books_in_api
 from application.users.bookrentals import BookRentals
 
 # get list of users in the app
-from application import users_table, books_record
+from application import users_table
 
 
 class Borrow(Resource):
@@ -18,28 +18,18 @@ class Borrow(Resource):
         try:
             # Let us load the book with the id given
             
-            book = books_in_api[int(book_id)]
-            
-            # Set the book to be unavailable
-            book['available'] = False
+            book = books_in_api[int(book_id)] 
+
             # Get the username of user who send the request
             username = request.json['username']
 
             # confirm user has an account with us
             for key in users_table:
                 if users_table[key]['username'] == username:
-                   # create a new row with details of the username and
-                   # bookid,etc
-                    rentals = BookRentals(
-                        username, book_id, 'to be set', 'to be set')
-
-                    # save the borrowing record to our table.
-                    books_record[len(books_record) + 1] = rentals.getdetails()
-
-                    # Notify the user
-                    books = rentals.getdetails()
-                    books['book_id'] = book_id
-                    return books, 201
+                    # Set the book to be unavailable
+                    book['available'] = False
+                    book['user_id'] = users_table[key]['id']
+                    return books, 200
 
                 else:
                     return {"Message": "No user with the username provided"}, 404
