8497c15a3f692f578cceb4217c25a9356dfe9c54
Prince <=>
2018-04-03 22:20:58 +0300

    [Feature #156479106] Add user_id to Book model.

--- application/books/models.py
+++ application/books/models.py
@@ -3,7 +3,8 @@ class Book(object):
     def __init__(self, title, author):
         self.author = author
         self.title = title
+        self.user_id = None
         self.available = True
 
     def getdetails(self):
-        return ({'title': self.title, 'author': self.author, 'available': self.available})
+        return ({'title': self.title, 'author': self.author,'user_id' : self.user_id, 'available': self.available})
