fd166180cb18d7f7751fd606a90cbf29c39e6db3
Prince <=>
2018-04-05 11:11:52 +0300

    Change configurations to staging

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,7 @@ from flask_restful import Api
 api = Api(app)
 
 from instance.config import configuration
-app.config.from_object(configuration['testing'])
+app.config.from_object(configuration['staging'])
 jwt = JWTManager(app)
 
 # To be used for storing blacklisted tokens
